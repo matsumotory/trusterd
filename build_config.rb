295da0f0968a30d003a538ca286a2ae578027afb
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2014-03-25 17:06:15 +0900

    Add rexep and file/io mrbgem

--- build_config.rb
+++ build_config.rb
@@ -20,6 +20,9 @@ MRuby::Build.new do |conf|
   # conf.gem :git => 'git@github.com:masuidrive/mrbgems-example.git', :branch => 'master', :options => '-v'
 
   conf.gem :github => 'matsumoto-r/mruby-http2'
+  conf.gem :github => 'iij/mruby-io'
+  conf.gem :github => 'iij/mruby-dir'
+  conf.gem :github => 'mattn/mruby-onig-regexp'
 
   # include the default GEMs
   conf.gembox 'default'
