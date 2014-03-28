a5356b07e8db3d5160bb6e52ab1fffca19d7625f
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2014-03-28 16:16:52 +0900

    Update config

--- README.md
+++ README.md
@@ -25,11 +25,23 @@ root_dir = "/usr/local/trusterd"
 
 s = HTTP2::Server.new({
 
+  #
+  # required config
+  #
+
   :port           => 8080,
-  #:key            => "#{root_dir}/ssl/server.key",
-  #:crt            => "#{root_dir}/ssl/server.crt",
   :document_root  => "#{root_dir}/htdocs",
   :server_name    => "mruby-http2 server",
+
+  # required when tls option is true.
+  # tls option is true by default.
+
+  :key            => "#{root_dir}/ssl/server.key",
+  :crt            => "#{root_dir}/ssl/server.crt",
+
+  # listen ip address
+  # default value is 0.0.0.0
+  # :server_host  => "127.0.0.1",
 
   #
   # optional config
@@ -39,7 +51,7 @@ s = HTTP2::Server.new({
   # :debug  =>  true,
 
   # tls default: true
-  :tls => false,
+  # :tls => false,
 
   # damone default: false
   # :daemon => true,
@@ -47,8 +59,9 @@ s = HTTP2::Server.new({
   # callback default: false
   # :callback => true,
 
-  # connection record default: true
+  # connection_record defualt: true
   # :connection_record => false,
+
 })
 
 #
--- conf/trusterd.conf.rb
+++ conf/trusterd.conf.rb
@@ -1,12 +1,51 @@
+#
+# Trusterd - HTTP/2 Web Server scripting with mruby
+#
+# Copyright (c) MATSUMOTO, Ryosuke 2014 -
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+# [ MIT license: http://www.opensource.org/licenses/mit-license.php ]
+#
+
 root_dir = "/usr/local/trusterd"
 
 s = HTTP2::Server.new({
 
+  #
+  # required config
+  #
+
   :port           => 8080,
-  #:key            => "#{root_dir}/ssl/server.key",
-  #:crt            => "#{root_dir}/ssl/server.crt",
   :document_root  => "#{root_dir}/htdocs",
   :server_name    => "mruby-http2 server",
+
+  # required when tls option is true.
+  # tls option is true by default.
+
+  :key            => "#{root_dir}/ssl/server.key",
+  :crt            => "#{root_dir}/ssl/server.crt",
+
+  # listen ip address
+  # default value is 0.0.0.0
+  # :server_host  => "127.0.0.1",
 
   #
   # optional config
@@ -16,7 +55,7 @@ s = HTTP2::Server.new({
   # :debug  =>  true,
 
   # tls default: true
-  :tls => false,
+  # :tls => false,
 
   # damone default: false
   # :daemon => true,
@@ -24,8 +63,9 @@ s = HTTP2::Server.new({
   # callback default: false
   # :callback => true,
 
-  # connection record default: true
+  # connection_record defualt: true
   # :connection_record => false,
+
 })
 
 #
