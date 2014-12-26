cc00c17bbc2e544cc93d4c2c56a6d305fc2f6ea1
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2014-12-26 05:55:06 -0800

    Add mruby-pack

--- build_config.rb
+++ build_config.rb
@@ -24,6 +24,7 @@ MRuby::Build.new do |conf|
   conf.gem :github => 'iij/mruby-io'
   conf.gem :github => 'iij/mruby-dir'
   conf.gem :github => 'iij/mruby-socket'
+  conf.gem :github => 'iij/mruby-pack'
   conf.gem :github => 'mattn/mruby-onig-regexp'
   conf.gem :github => 'mattn/mruby-json'
 
