d23c652cbea2ee3680efd6eb8e43c3b3d95f7233
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2014-12-26 21:15:58 -0800

    Use jemalloc

--- build_config.rb
+++ build_config.rb
@@ -48,17 +48,17 @@ MRuby::Build.new do |conf|
   # end
 
   # Linker settings
-  # conf.linker do |linker|
+   conf.linker do |linker|
   #   linker.command = ENV['LD'] || 'gcc'
   #   linker.flags = [ENV['LDFLAGS'] || []]
   #   linker.flags_before_libraries = []
-  #   linker.libraries = %w()
+     linker.libraries << 'jemalloc'
   #   linker.flags_after_libraries = []
   #   linker.library_paths = []
   #   linker.option_library = '-l%s'
   #   linker.option_library_path = '-L%s'
   #   linker.link_options = "%{flags} -o %{outfile} %{objs} %{libs}"
-  # end
+   end
 
   # Archiver settings
   # conf.archiver do |archiver|
