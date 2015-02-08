e02eb3192aabb4d5eb513d7a73641cc5b4481640
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2015-02-08 06:32:52 -0800

    Update mini_trusterd.c

--- src/mini_trusterd.c
+++ src/mini_trusterd.c
@@ -2,20 +2,44 @@
 #include "mruby.h"
 #include "mruby/compile.h"
 
-const char config[] = "                                       \
-                       HTTP2::Server.new({                    \
-                           :port => 8080,                     \
-                           :document_root => './',            \
-                           :server_name => 'mini_trusterd',   \
-                           :tls => false,                     \
-                           :worker => 'auto',                 \
-                       }).run                                 \
+const char config[] = "                                       \n\
+                       s = HTTP2::Server.new({                \n\
+                           :port => port,                     \n\
+                           :document_root => './',            \n\
+                           :server_name => 'mini_trusterd',   \n\
+                           :tls => false,                     \n\
+                           :callback => true,                 \n\
+                           :worker => 'auto',                 \n\
+                       })                                     \n\
+";
+
+const char response[] = "                                     \n\
+                       s.set_map_to_strage_cb {               \n\
+                         if s.request.uri =~ /hello/          \n\
+                           s.set_content_cb {                 \n\
+                             s.rputs 'hello mini-trusterd '   \n\
+                             s.echo 'world from cb.'          \n\
+                           }                                  \n\
+                         end                                  \n\
+                       }                                      \n\
+";
+
+const char run[] = "                                          \n\
+                       s.run                                  \n\
 ";
 
 int main(void)
 {
   mrb_state *mrb = mrb_open();
-  mrb_load_string(mrb, config);
+  mrbc_context *ctx = mrbc_context_new(mrb);
+
+  // 8080 into local variable "port" on same ctx
+  mrb_load_string_cxt(mrb, "port = 8080", ctx);
+  mrb_load_string_cxt(mrb, config, ctx);
+  mrb_load_string_cxt(mrb, response, ctx);
+  mrb_load_string_cxt(mrb, run, ctx);
+
+  mrbc_context_free(mrb, ctx);
   mrb_close(mrb);
   return 0;
 }
