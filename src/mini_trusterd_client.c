86af5a85fb58df79ec4f6296b0dc6dd54beca19f
MATSUMOTO, Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2015-02-10 23:33:52 +0900

    Update mini_trusterd_client.c

--- src/mini_trusterd_client.c
+++ src/mini_trusterd_client.c
@@ -11,7 +11,7 @@ int main(void)
   mrb_state *mrb = mrb_open();
   mrbc_context *ctx = mrbc_context_new(mrb);
 
-  // 8080 into instance variable "@url" of main on same ctx
+  // URL into instance variable "@url" of main on same ctx
   mrb_iv_set(mrb, mrb_top_self(mrb), mrb_intern_lit(mrb, "@url"), mrb_str_new_lit(mrb, "https://127.0.0.1:8080/index.html"));
   response = mrb_load_string_cxt(mrb, request, ctx);
   mrb_funcall(mrb, mrb_top_self(mrb), "pp", 1, response);
