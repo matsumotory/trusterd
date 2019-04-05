fe63e3d7447c3e38cf1978bfae5f3b1b8ceeb1f7
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2019-04-05 13:14:23 +0900

    Add build dependency : libcurl

--- misc/provision.sh
+++ misc/provision.sh
@@ -5,7 +5,7 @@ sudo apt-get -y install build-essential rake bison git gperf automake m4 \
                 autoconf libtool cmake pkg-config libcunit1-dev ragel \
                 libpcre3-dev clang-format-6.0 \
                 libev-dev libevent-dev libjansson-dev libjemalloc-dev \
-                libxml2-dev libssl-dev zlib1g-dev libc-ares-dev
+                libxml2-dev libssl-dev zlib1g-dev libc-ares-dev libcurl4-openssl-dev
 sudo apt-get -y remove nano
 
 sudo update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/clang-format-6.0 1000
